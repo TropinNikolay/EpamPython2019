fe4fb74acb092d09d850a63aa0b7baea7bc8d009
TropinNikolay <47924587+TropinNikolay@users.noreply.github.com>
2019-11-07 23:21:51 +0300

    Solved homework task 1

--- 00-Introduction-to-Python/hw.py
+++ 00-Introduction-to-Python/hw.py
@@ -7,8 +7,18 @@
 являются существенными.
 """
 
+
 def is_permutation(a: str, b: str) -> bool:
     # Нужно проверить, являются ли строчки 'a' и 'b' перестановками
+
+    if len(a) != len(b):
+        return False
+
+    marker = True
+    for i in a:
+        if a.count(i) != b.count(i):
+            marker = False
+    return marker
 
 
 assert is_permutation('baba', 'abab')
