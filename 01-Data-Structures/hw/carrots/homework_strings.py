b8136e7e0899a814b60632ae1097ed86c2e020a5
TropinNikolay <47924587+TropinNikolay@users.noreply.github.com>
2019-11-16 22:26:38 +0300

    Added solution for homework "carrots"

--- 01-Data-Structures/hw/carrots/homework_strings.py
+++ 01-Data-Structures/hw/carrots/homework_strings.py
@@ -33,26 +33,84 @@ P.S. За незакрытый файловый дескриптор - кара�
 
 """
 
-# read the file dna.fasta
-dna = None
+import matplotlib.pyplot as plt
+import numpy as np
+from Bio import SeqIO
 
 
 def translate_from_dna_to_rna(dna):
-    
-    """your code here"""
-    
+    rna = dna.seq.complement().transcribe()
     return rna
 
 
 def count_nucleotides(dna):
-    
-    """your code here"""
-    
+    num_of_nucleotides = {i: dna.seq.count(i) for i in ['A', 'C', 'G', 'T']}
     return num_of_nucleotides
 
 
 def translate_rna_to_protein(rna):
-    
-    """your code here"""
-    
+    protein = rna.translate(stop_symbol='Stop')
     return protein
+
+
+# this is necessary for histograma
+def autolabel(rects):
+    """ Attach a text label above each bar in *rect*, displaying its height """
+    for rect in rects:
+        height = rect.get_height()
+        ax.annotate('{}'.format(height),
+                    xy=(rect.get_x() + rect.get_width() / 2, height),
+                    xytext=(0, 3),
+                    textcoords="offset points",
+                    ha='center', va='bottom')
+
+
+dna = None
+iteration = 0  # needs for saving number of nucleotides
+for dna in SeqIO.parse('files/dna.fasta', 'fasta'):
+    rna = translate_from_dna_to_rna(dna)
+
+    nucleotides = count_nucleotides(dna)
+    if iteration == 0:
+        dna_one_means = [i for i in nucleotides.values()]
+    else:
+        dna_second_means = [i for i in nucleotides.values()]
+
+    with open('nucleotides.txt', 'a') as inf_1, open('RNA.txt', 'a') as inf_2, open('protein.txt', 'a') as inf_3:
+        inf_1.write(dna.description + '\n')
+        for key, value in nucleotides.items():
+            inf_1.write(key + ' => ' + str(value) + '\n')
+        inf_1.write('\n')
+
+        inf_2.write(dna.description + '\n')
+        inf_2.write('RNA: ' + str(rna) + '\n')
+        inf_2.write('\n')
+
+        inf_3.write(dna.description + '\n')
+        inf_3.write('protein: ' + str(translate_rna_to_protein(rna)) + '\n')
+        inf_3.write('\n')
+
+    iteration += 1
+
+# now let's draw our histograma
+labels = ['A', 'C', 'G', 'T']
+
+x = np.arange(len(labels))  # the label locations
+width = 0.35  # the width of the bars
+
+fig, ax = plt.subplots()
+rect1 = ax.bar(x - width / 2, dna_one_means, width, label='HSGLTH1')
+rect2 = ax.bar(x + width / 2, dna_second_means, width, label='HSBGPG')
+
+ax.set_ylabel('Number of nucleotides')
+ax.set_title('Number of nucleotides by group and type')
+ax.set_xticks(x)
+ax.set_xticklabels(labels)
+ax.legend()
+
+autolabel(rect1)
+autolabel(rect2)
+
+fig.tight_layout()
+
+plt.show()
