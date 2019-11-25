ec42991c8a9a766faf364e10294183798beb69ac
TropinNikolay <47924587+TropinNikolay@users.noreply.github.com>
2019-11-26 00:33:14 +0300

    Some bug fixes in hw4

--- 02-Functions/hw/hw4.py
+++ 02-Functions/hw/hw4.py
@@ -3,17 +3,16 @@ import inspect
 
 def modified_func(func, *fixated_args, **fixated_kwargs):
     def wrapped(*args, **kwargs):
-        if fixated_args:
-            list(args).extend(list(fixated_args))
-        if fixated_kwargs:
-            kwargs.update(fixated_kwargs)
-        return func(*args, **kwargs)
+        if args:
+            list(fixated_args).extend(list(args))
+        if kwargs:
+            fixated_kwargs.update(kwargs)
+        return func(*fixated_args, **fixated_kwargs)
 
     wrapped.__name__ = "func_{}".format(func.__name__)
     wrapped.__doc__ = """\
-    A func implementation of {} with pre-applied arguments being: 
-    fixated_args: {} fixated_kwargs: {} 
-    source_code:\n{}""".format(wrapped.__name__, fixated_args, fixated_kwargs, inspect.getsource(func))
+    A func implementation of {} with pre-applied arguments being: {} 
+    source_code:\n{}""".format(wrapped.__name__, inspect.getcallargs(func), inspect.getsource(func))
     return wrapped
 
 
@@ -21,7 +20,7 @@ def example(a=10, b=10):
     return a + b
 
 
-omega = modified_func(example, 3, b=12)
+omega = modified_func(example, 4, 5)
 print(omega.__name__)
-print(omega(a=2, b=4))
+print(omega())
 print(omega.__doc__)
