a16296bae74a9b06f3de63d2d6dd83beebba72e6
TropinNikolay <47924587+TropinNikolay@users.noreply.github.com>
2019-12-11 17:38:38 +0300

    Added solved task1

--- 06-advanced-python/hw/task1.py
+++ 06-advanced-python/hw/task1.py
@@ -9,6 +9,34 @@ E - dict(<V> : [<V>, <V>, ...])
 class Graph:
     def __init__(self, E):
         self.E = E
+        self.visited = list()
+        self.length = len(self.E)
+        self.counter = 0
+
+    def bfs(self):
+        start = list(self.E.keys())[0]
+        queue = [start]
+        while queue:
+            vertex = queue.pop(0)
+            if vertex not in self.visited:
+                self.visited.append(vertex)
+                tmp = []
+                for ver in self.E[vertex]:
+                    if ver not in self.visited:
+                        tmp.append(ver)
+                queue.extend(tmp)
+        return self.visited
+
+    def __next__(self):
+        if self.counter < self.length:
+            self.counter += 1
+            self.bfs()
+            return self.visited[self.counter - 1]
+        else:
+            raise StopIteration
+
+    def __iter__(self):
+        return self
 
 
 E = {'A': ['B', 'C', 'D'], 'B': ['C'], 'C': [], 'D': ['A']}
