9ccee6a38a96ddb4ee0b869c38bc4783c79b4512
TropinNikolay <47924587+TropinNikolay@users.noreply.github.com>
2019-12-12 00:16:58 +0300

    Added solved task3

--- 06-advanced-python/hw/task3.py
+++ 06-advanced-python/hw/task3.py
@@ -7,4 +7,18 @@ print("It's fine")
 
 
 class Suppressor:
-    pass
+    def __init__(self, *args, **kwargs):
+        self.exceptions = args
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if issubclass(exc_type, self.exceptions):
+            print('Suppressing', exc_val)
+            return True
+
+
+with Suppressor(ZeroDivisionError):
+    1 / 0
+print("It's fine")
