2bd31ce05656b7ec7d6b7dce34630324dba3115a
TropinNikolay <47924587+TropinNikolay@users.noreply.github.com>
2019-12-12 22:38:07 +0300

    Name fixed

--- 04-OOP/hw/save_original_info.py
+++ 04-OOP/hw/save_original_info.py
@@ -19,7 +19,7 @@ print(custom_sum.__original_func)  # <function custom_sum at <some_id>>
 import functools
 
 
-def decorator(func):
+def save_original_info(func):
     def wrapper(function):
         def inner(*args, **kwargs):
             inner.__doc__ = func.__doc__
@@ -33,7 +33,7 @@ def decorator(func):
 
 
 def print_result(func):
-    @decorator(func)
+    @save_original_info(func)
     def wrapper(*args, **kwargs):
         """Function-wrapper which print result of an original function"""
         result = func(*args, **kwargs)
