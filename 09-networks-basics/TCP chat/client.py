1e449f28296ccc9cca6ccd5695d3c99c4f7a33dd
TropinNikolay <47924587+TropinNikolay@users.noreply.github.com>
2019-12-17 03:32:46 +0300

    Added separate folder and black formatting

--- 09-networks-basics/TCP chat/client.py
+++ 09-networks-basics/TCP chat/client.py
@@ -13,13 +13,16 @@ def receive(client):
 
 NEW = True
 CLIENT = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-CLIENT.connect(('localhost', 5090))
+CLIENT.connect(("localhost", 5090))
 BUFSIZE = 1024
 threading.Thread(target=receive, args=(CLIENT,)).start()
-print('Welcome to TCP chat!',
-      'If you want to get full member list use -l',
-      'If you want to send a message to a specific user, type his name, then -u, then your message',
-      'If you want to quit type q', sep='\n')
+print(
+    "Welcome to TCP chat!",
+    "If you want to get full member list use -l",
+    "If you want to send a message to a specific user, type his name, then -u, then your message",
+    "If you want to quit type q",
+    sep="\n",
+)
 
 while True:
     try:
@@ -30,10 +33,10 @@ while True:
         else:
             DATA = input()
             CLIENT.send(DATA.encode("utf-8"))
-            if DATA == 'q':
+            if DATA == "q":
                 CLIENT.close()
                 break
     except KeyboardInterrupt:
-        CLIENT.send(b'q')
+        CLIENT.send(b"q")
         CLIENT.close()
         break
--- 09-networks-basics/TCP chat/server.py
+++ 09-networks-basics/TCP chat/server.py
@@ -13,28 +13,28 @@ def handle_client(client_socket):
     try:
         name = client_socket.recv(BUFSIZE).decode()
         clients[client_socket] = name
-        broadcast(name, client_socket, 'new')
+        broadcast(name, client_socket, "new")
         print("Data received:", name)
         while True:
             data = client_socket.recv(BUFSIZE).decode()
             if data == "q":
                 print("Client quits")
-                broadcast(data, client_socket, 'q')
+                broadcast(data, client_socket, "q")
                 client_socket.close()
                 del clients[client_socket]
                 break
-            elif data == '-l':
-                users = ''
+            elif data == "-l":
+                users = ""
                 for client in clients.values():
-                    users += str(client) + '\n'
+                    users += str(client) + "\n"
                 client_socket.send(users.encode())
-            elif data.find('-u') != -1:
-                index = data.find('-u')
-                name = data[:index - 1]
-                message = data[index + 3:]
+            elif data.find("-u") != -1:
+                index = data.find("-u")
+                name = data[: index - 1]
+                message = data[index + 3 :]
                 for address, client_name in clients.items():
                     if client_name == name:
-                        message = f'[{str(clients[client_socket])}] {message}'
+                        message = f"[{str(clients[client_socket])}] {message}"
                         address.send(message.encode())
             else:
                 broadcast(data, client_socket)
@@ -47,18 +47,18 @@ def handle_client(client_socket):
             print("Client quits")
 
 
-def broadcast(data, client_socket, flag=''):
+def broadcast(data, client_socket, flag=""):
     for client in clients:
         if client != client_socket:
             try:
-                if flag == 'q':
-                    message = f'{str(clients[client_socket])} has left the chat'
+                if flag == "q":
+                    message = f"{str(clients[client_socket])} has left the chat"
                     client.send(message.encode())
-                elif flag == 'new':
-                    message = f'{str(clients[client_socket])} has joined the chat!'
+                elif flag == "new":
+                    message = f"{str(clients[client_socket])} has joined the chat!"
                     client.send(message.encode())
                 else:
-                    message = f'[{str(clients[client_socket])}] {data}'
+                    message = f"[{str(clients[client_socket])}] {data}"
                     client.send(message.encode())
             except OSError:
                 pass
@@ -66,14 +66,14 @@ def broadcast(data, client_socket, flag=''):
 
 clients = {}
 
-HOST = 'localhost'
+HOST = "localhost"
 PORT = 5090
 BUFSIZE = 1024
 
 SERVER = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 SERVER.bind((HOST, PORT))
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     SERVER.listen(100)
     print("Server is running on port 5090")
     print("Waiting for connection...")
