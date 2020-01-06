65417beb0bfe0e46de7d4be48d2df0e1becfbaa8
TropinNikolay <47924587+TropinNikolay@users.noreply.github.com>
2020-01-06 03:38:16 +0300

    Added solution for task4

--- 06-advanced-python/hw/task4.py
+++ 06-advanced-python/hw/task4.py
@@ -11,21 +11,44 @@ V folder1
 > print(file3 in folder2)
 True
 """
+import os
 
 
 class PrintableFolder:
-    def __init__(self, name, content):
-        self.name = name
-        self.content = content
+    def __init__(self, path):
+        self.path = path
 
     def __str__(self):
-        pass
+        result = ""
+        for root, dirs, files in os.walk(self.path):
+            level = root.replace(self.path, '').count(os.sep)
+            indent = "|\t" * (level - 1)
+
+            if level == 0:
+                result += f'{indent}V {os.path.basename(root)}\n'
+            else:
+                result += f'{indent}|-> V {os.path.basename(root)}\n'
+
+            subindent = "|\t" * level
+            for file in files:
+                result += f'{subindent}|-> {file}\n'
+
+        return result
+
+    def __contains__(self, item):
+        for elements in os.walk(self.path):
+            if item in elements[1] + elements[2]:
+                return True
+        return False
 
 
-class PrintableFile:
-    def __init__(self, name):
-        self.name = name
-
-    def __str__(self):
-        pass
-
+if __name__ == "__main__":
+    folder1 = PrintableFolder("E:\\GitHub\\EpamPython2019\\07-python-for-advanced")
+    folder2 = PrintableFolder("E:\\GitHub\\EpamPython2019\\06-advanced-python")
+    print(folder1)
+    print(folder2)
+    file1 = "task1.py"
+    file2 = "task2.py"
+    file3 = "task3.py"
+    print(file1 in folder2)
+    print(file2 in folder1)
